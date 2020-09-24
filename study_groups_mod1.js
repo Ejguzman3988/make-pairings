ae5490490bc2b2966c5b3744f17de96a7c7e91e4
curlywallst <nnoyes@comcast.net>
2020-09-24 18:55:01 -0400

    saving for computer wipe

---
+++ study_groups_mod1.js
@@ -0,0 +1,45 @@
+// WEEK 1 EQUALS THIS WEEK
+
+var sgData = [
+	{
+		categories: [],
+		title: '[0908 Full Time] Live Lecture - Object Relationships',
+		description: 'Come join.',
+		day: 'Tuesday',
+		time: '11:00 am',
+		weekToStart: 3,
+		weekToEnd: 3,
+		zoom: 'https://flatironschool.zoom.us/j/93581559641?pwd=R09wZThzTGtUMmY5ck84OTkwZFhIdz09',
+    },
+    {
+		categories: [],
+		title: '[0908 Full Time] Live Lecture - Accessing Data From Remote Resources (APIs)',
+		description: 'Come join.',
+		day: 'Wednesday',
+		time: '11:00 am',
+		weekToStart: 3,
+		weekToEnd: 3,
+		zoom: 'https://flatironschool.zoom.us/j/93581559641?pwd=R09wZThzTGtUMmY5ck84OTkwZFhIdz09',
+    },
+    {
+		categories: [],
+		title: '[0908 Full Time] Live Discussion - Build a CLI Application w Scraper or API',
+		description: 'Come join.',
+		day: 'Thursday',
+		time: '11:00 am',
+		weekToStart: 3,
+		weekToEnd: 3,
+		zoom: 'https://flatironschool.zoom.us/j/93581559641?pwd=R09wZThzTGtUMmY5ck84OTkwZFhIdz09',
+    },
+    {
+		categories: [],
+		title: '[0908 Full Time] Open Office Hours',
+		description: 'Come join.',
+		day: 'Friday',
+		time: '11:00 am',
+		weekToStart: 3,
+		weekToEnd: 3,
+		zoom: 'https://flatironschool.zoom.us/j/93581559641?pwd=R09wZThzTGtUMmY5ck84OTkwZFhIdz09',
+    }
+];
+
